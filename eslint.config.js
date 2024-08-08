ed1ef580c7c00afc663901cefdd3f23cf5dc5460
Vlad <vlad.herasymovychh@gmail.com>
2024-08-08 08:26:04 +0200

    build: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import prettierConfig from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-multi-spaces': ['error'],
+      'vue/multi-word-component-names': 0,
+      'vue/no-restricted-component-options': [
+        'error',
+        {
+          name: 'component',
+          message: 'Use "components" instead.',
+        },
+      ],
+      'vue/html-indent': [
+        'error',
+        2,
+        {
+          attribute: 1,
+          closeBracket: 0,
+          alignAttributesVertically: true,
+          ignores: [],
+        },
+      ],
+    },
+  },
+  prettierConfig,
+  eslintPluginPrettierRecommended,
+];
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="6" class="text-center">
+        <span>App</span>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
